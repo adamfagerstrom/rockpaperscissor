27cc9c29f30faa880f3392b180dc44e447d96466
Adam Fagerström <adamfagerstrom@gmail.com>
2020-12-18 11:56:23 +0100

    Changes

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ function game() {
             computerScore
         );
     } else {
-        console.log("It´s a tie... Och vi testar branch");
+        console.log("It´s a tie... Och vi testar branch och mergar den");
     }
 }
 game();
